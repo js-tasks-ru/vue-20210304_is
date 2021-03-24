23522ae3469c1b103ef6d668f36567fd2fe2dd32
Sergey Butyanov <igor@tomatstudio.ru>
2021-03-24 16:03:44 +0300

    Meetup Info

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,52 @@
 export default {
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true
+    },
+    place: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    }
+  },
+
+  computed: {
+    localDateText() {
+      let result = new Date(this.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+
+      return result
+    },
+
+    localDateAttribute() {
+      let result = new Date(this.date).toISOString().split('T')[0];
+
+      return result
+    }
+  },
+
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="localDateAttribute">{{localDateText}}</time>
       </li>
     </ul>`,
 };
